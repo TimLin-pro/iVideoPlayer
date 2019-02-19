984db26ce185ec1e4e92db27a82795ef9eead3aa
ivanljt <596476418@qq.com>
2019-02-19 18:02:47 +0800

    feat:display video list when click folder in FileListFragment refactor:rename some file and method

--- app/src/main/java/com/android/timlin/ivedioplayer/MainActivity.java
+++ app/src/main/java/com/android/timlin/ivedioplayer/MainActivity.java
@@ -1,9 +1,7 @@
 package com.android.timlin.ivedioplayer;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.android.timlin.ivedioplayer.list.file.FileListFragment;
 
@@ -18,18 +16,5 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .add(R.id.content_main, fileListFragment)
                 .commit();
-
-        new AlertDialog.Builder(this)
-                .setPositiveButton("aaa", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                }).setNegativeButton("", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
     }
 }
